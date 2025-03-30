3ea3cba91d2e40a179e883b1141f2836673e383a
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2025-03-30 19:18:10 +0500

    add env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() =>{
     try {
-        await mongoose.connect('mongodb+srv://nxtgenwebsites:DKXOMCeEaleBXLVG@healthcarefacilities.6zml0.mongodb.net/health-facility');
+        await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB connected successfully!`);
     } catch (err) {
         console.error("MongoDB connection error:", err)
